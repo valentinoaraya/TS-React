7cba5a3a8ed94d22af5997c5ca92f1155ab25cb4
¨Tino¨ <valentinoaraya04@gmail.com>
2023-02-05 21:33:38 -0300

    ¨Thikia-Sport-2.1.4¨

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
         <div className='redes'>
             <div className='logoRed'>
                 <a href="https://www.instagram.com/thikiasport/" target={"_blank"} rel="noopener noreferrer">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="#f4cb00" className="bi bi-facebook" viewBox="0 0 16 16">
                     <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
                 </svg>
                 </a>
@@ -23,7 +23,7 @@ function Footer() {
         </div>
         <p className='copy'>© Copyright by Valentino Araya</p>
         <p className='copy'>Powered by React JS</p>
-        <p className='copy'>Thikia Sport 2.1.3</p>
+        <p className='copy'>Thikia Sport 2.1.4</p>
     </footer>
   )
 }
